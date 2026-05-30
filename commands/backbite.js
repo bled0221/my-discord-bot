e419798f0bf3dd468c3a421b871c3fcdb937902d
bled0221 <firefox8979@gmail.com>
2026-05-30 15:19:24 +0900

    Design: 다크/라이트 모드 자동 매칭 투명 색상(1) 적용

--- commands/backbite.js
+++ commands/backbite.js
@@ -46,7 +46,7 @@ module.exports = {
 
         // 익명 박스(임베드) 꾸미기
         const anonymousEmbed = new EmbedBuilder()
-            .setColor('#Default')
+            .setColor(1)
             .setTitle('익명 뒷담 메시지')
             .setDescription(`"${content}"`)
             .setTimestamp()
